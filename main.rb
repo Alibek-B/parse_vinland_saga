e82fc6afc8b3db8a2acb9cf5d99d04b388eababb
baim <baim_a@outlook.com>
2024-08-25 13:53:42 +0300

    fix url

--- main.rb
+++ main.rb
@@ -3,9 +3,13 @@
 require_relative "lib/parse"
 require_relative "lib/create_pdf"
 
-URL_WITHOUT_CHAPTER_NUMBER = "https://www.vinlandsagamanga.net/manga/vinland-saga-chapter-"
+# valid url
+# https://www.vinlandsagamanga.net/manga/vinland-saga-chapter-30/
+URL_WITHOUT_CHAPTER_NUMBER = "https://www.vinlandsagamanga.net/manga/vinland-saga-chapter"
 
 print "Send number of chapeter: "
 number = gets.chomp
+
 url = "#{URL_WITHOUT_CHAPTER_NUMBER}-#{number}/"
-CreatePdf.new(url, filename:"vinland-saga-chapter-#{number}.pdf").create
+
+CreatePdf.new(url, "vinland-saga-chapter-#{number}.pdf").create
